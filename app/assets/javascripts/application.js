8319707c2397afd721842f9cde143b2cbb46db06
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2018-09-13 22:59:44 -0300

    Fixed bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+//= require bootstrap
+//= require bootstrap-datepicker
+//= require bootstrap-datetimepicker
 //= require_tree .
