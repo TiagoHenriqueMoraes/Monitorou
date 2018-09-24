e5036df1a4edf9eec4d158f5bbe931a47343844e
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2018-09-24 19:30:57 -0300

    Created confirmable email for user

--- db/schema.rb
+++ db/schema.rb
@@ -39,9 +39,9 @@ ActiveRecord::Schema.define(version: 2018_09_11_122602) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.integer "kind", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "kind", default: 0
     t.bigint "worktime_id"
     t.bigint "subject_id"
     t.bigint "course_id"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 2018_09_11_122602) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["course_id"], name: "index_users_on_course_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
